43c7fc7cb4e45ef0985b6e7988b43a9e67b56413
Carlos Pereira <carlos.pereira@tekever.com>
2024-03-12 19:25:33 +0000

    Add crawler

--- backend/crawler.py
+++ backend/crawler.py
@@ -1,17 +1,29 @@
-# from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
 base_url = "https://www.codigo-postal.pt/"
 
 first = input("first ")
 second = input("second ")
 
-# https://www.codigo-postal.pt/?cp4=4400&cp3=074
+
 def search_address(postal_code_first_part, postal_code_second_part):
     request_url = f"{base_url}?cp4={postal_code_first_part}&cp3={postal_code_second_part}"
-    r = requests.get(request_url)
-    print(r.status_code)
-    return request_url
+    response = requests.get(request_url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        address_element = soup.find("p", class_="odd")
+
+        if address_element:
+            address_content = address_element.text.strip()
+        else:
+            address_content = "Not found"
+
+        return address_content
+
+    else:
+        raise RuntimeError("Erro ao buscar endereço: {}".format(response.status_code))
 
 
-print(search_address(first, second))
+search_address(first, second)
