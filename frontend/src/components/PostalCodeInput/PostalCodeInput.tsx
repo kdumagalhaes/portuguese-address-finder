d4b1baebb4adba82f26aac3b8ef1fe16ff6ddfd9
Carlos Pereira <carlos.pereira@tekever.com>
2024-03-13 20:45:52 +0000

    Add backend integration

--- frontend/src/components/PostalCodeInput/PostalCodeInput.tsx
+++ frontend/src/components/PostalCodeInput/PostalCodeInput.tsx
@@ -5,12 +5,43 @@ import {
   InputOTPSlot,
   InputOTPSeparator
 } from "@/components/ui/input-otp"
+import { useEffect, useState } from "react"
+
+interface PostalCode {
+  firstPart: string,
+  secondPart: string
+}
+
+const BASE_URL = "http://localhost:3000/api/search-address?cep="
  const PostalCodeInput = () => {
+ const [postalCodeInput, setPostalCodeInput] = useState("")
+
+ const normalizePostalCode = (postalCodeValue: string) => {
+      const firstPart = postalCodeValue.slice(0, 4)
+      const secondPart = postalCodeValue.slice(4, 7)
+      fetchPostalCode({firstPart, secondPart})
+ }
+
+ const fetchPostalCode = async (postalCode: PostalCode) => {
+      const response = await fetch(`${BASE_URL}${postalCode.firstPart}-${postalCode.secondPart}`);
+      const data = await response.json();
+      console.log("data = ", data)
+ }
+
+ useEffect(() => {
+  if (postalCodeInput.length === 7) {
+    normalizePostalCode(postalCodeInput)
+  }
+ // eslint-disable-next-line react-hooks/exhaustive-deps
+ }, [postalCodeInput])
+
 
   return (
     <InputOTP
     maxLength={7}
     pattern={REGEXP_ONLY_DIGITS}
+    onChange={(value) => setPostalCodeInput(value)}
+    autoFocus
     render={({ slots }) => (
       <>
         <InputOTPGroup>
