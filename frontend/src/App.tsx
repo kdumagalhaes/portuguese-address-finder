72e57a3de68c11959c9125446fc9209f5c89d85c
Carlos Pereira <carlos.pereira@tekever.com>
2024-03-13 22:02:53 +0000

    Add context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
+import Address from './components/Address/Address'
 import PostalCodeInput from './components/PostalCodeInput/PostalCodeInput'
+import { AddressProvider } from './context/AddressContext'
 
 function App() {
 
   return (
-    <div className="h-screen flex items-center justify-center">
-    <PostalCodeInput />
-    </div>
+    <div className="h-screen flex items-center  flex-col gap-6">
+        <AddressProvider>
+        <PostalCodeInput />
+        <Address />
+      </AddressProvider>
+      </div>
   )
 }
 
---
+++ frontend/src/context/AddressContext.tsx
@@ -0,0 +1,45 @@
+import { ReactNode, createContext, useContext, useState } from "react";
+
+interface AddressProviderProps {
+  children: ReactNode
+}
+
+interface AddressContextModel {
+  address: Address,
+  getAddress: (address: Address) => void
+}
+
+interface Address {
+  street: string,
+  local: string
+}
+
+const AddressContext = createContext({} as AddressContextModel)
+
+export const AddressProvider = ({children}: AddressProviderProps) => {
+
+  const [address, setAddress] = useState<Address>({street: "", local: ""})
+
+  const getAddress = (address: Address) => {
+    setAddress(address)
+  }
+
+  const value = {
+    getAddress,
+    address
+  }
+
+  return <AddressContext.Provider value={value}>{children}</AddressContext.Provider>
+
+}
+
+export const useAddress = () => {
+  const context = useContext(AddressContext)
+
+  if (context === undefined) {
+    throw new Error('useAddress must be used within AddressContext')
+
+  }
+
+  return context
+}
